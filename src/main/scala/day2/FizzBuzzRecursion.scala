d21ea1a3dafe81e2c194cfdc14ff08f6cc920885
dannywaughman <dannyjameswaughman@msn.com>
2020-03-03 12:32:30 +0000

    FizzBuzzRecursion added

---
+++ src/main/scala/day2/FizzBuzzRecursion.scala
@@ -0,0 +1,27 @@
+package day2
+// Rewrite your previous three iteration exercises but using method recursion, no loops
+//involved.
+object FizzBuzzRecursion extends App {
+
+  def fizzBuzzRecursion(strOne:String,strTwo:String,num:Int):Unit ={
+
+
+    if (num % 3 == 0 && num % 5 != 0){
+      println(strOne)
+    } else if (num % 5 == 0 && num % 3 != 0){
+      println(strTwo)
+    } else if (num % 3 == 0 && num % 5 == 0){
+      println(strOne + strTwo)
+    } else {
+      println(num)
+    }
+
+    val newNum = num - 1
+    if (num > 1) {
+      fizzBuzzRecursion(strOne, strTwo, newNum)
+    }
+  }
+
+  fizzBuzzRecursion("Fizz","Buzz",15)
+
+}
