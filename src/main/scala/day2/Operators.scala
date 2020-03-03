b9b48a2ea48ce6af3f14d0f5d168711d65b55430
dannywaughman <dannyjameswaughman@msn.com>
2020-03-03 11:49:16 +0000

    Operators added

---
+++ src/main/scala/day2/Operators.scala
@@ -0,0 +1,11 @@
+package day2
+// Create a method that accepts two Integers as an input, then returns the sum of those integers.
+object Operators extends App {
+
+  def add (a:Int,b:Int):Int = {
+    a + b
+  }
+
+  println(add(10,20))
+
+}
