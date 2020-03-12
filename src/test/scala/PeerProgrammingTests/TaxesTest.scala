ca1ff2fe3808b109f8687f6a3b61a46f25e3b4ff
dannywaughman <dannyjameswaughman@msn.com>
2020-03-12 12:29:47 +0000

    Peerprogramming taxes beautiful solution

--- src/test/scala/PeerProgrammingTests/TaxesTest.scala
+++ src/test/scala/PeerProgrammingTests/TaxesTest.scala
@@ -11,13 +11,33 @@ class TaxesTest extends FunSuite {
   test ("10k") {
     assert(Taxes.calculateTax(10000) === 0)
   }
+  test ("10010") {
+    assert(Taxes.calculateTax(10010) === 1)
+  }
   test ("30k") {
-    assert(Taxes.calculateTax(30000) === 3000)
+    assert(Taxes.calculateTax(30000) === 2000)
   }
-  test ("100k") {
-    assert(Taxes.calculateTax(100000) === 25000)
+  test ("56789") {
+    assert(Taxes.calculateTax(56789) === 8697)
   }
-  test ("1million") {
-    assert(Taxes.calculateTax(1000000) === 400000)
+  test ("1234567") {
+    assert(Taxes.calculateTax(1234567) === 473326)
   }
+
+
+  test ("overall 0"){
+    assert(Taxes.overall(0) === 0)
+  }
+  test ("overall 0.06"){
+    assert(Taxes.overall(0.06) === 25000)
+  }
+  test ("overall 0.09"){
+    assert(Taxes.overall(0.09) === 34372)
+  }
+  test ("overall 0.32"){
+    assert(Taxes.overall(0.32) === 256250)
+  }
+//  test ("overall 0"){
+//    assert(Taxes.overall(0.40) === assertTypeError)
+//  }
 }
