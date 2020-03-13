034261da2020fff22fac6f953edb816714d9949e
dannywaughman <dannyjameswaughman@msn.com>
2020-03-13 14:59:13 +0000

    Blackjack updated

--- src/main/scala/day6/BlackJack.scala
+++ src/main/scala/day6/BlackJack.scala
@@ -20,6 +20,8 @@ object BlackJack extends App {
   var game = true
   var winCount = 0
   var drawCount = 2
+  var dealerDrawCount = 2
+  val r = new scala.util.Random
 
   for (i <- 1 to 9) {
     cardList += (i, i, i, i)
@@ -29,13 +31,11 @@ object BlackJack extends App {
   }
 
   def dealerCards(): Int = {
-    val r1 = new scala.util.Random
-    firstCompCard = cardList(r1.nextInt(cardCount))
+    firstCompCard = cardList(r.nextInt(cardCount))
     cardCount -= 1
     cardList -= firstCompCard
 
-    val r2 = new scala.util.Random
-    secondCompCard = cardList(r2.nextInt(cardCount))
+    secondCompCard = cardList(r.nextInt(cardCount))
     cardCount -= 1
     cardList -= secondCompCard
 
@@ -58,13 +58,11 @@ object BlackJack extends App {
   }
 
   def playerCards(): Int = {
-    val r1 = new scala.util.Random
-    firstPlayerCard = cardList(r1.nextInt(cardCount))
+    firstPlayerCard = cardList(r.nextInt(cardCount))
     cardCount -= 1
     cardList -= firstPlayerCard
 
-    val r2 = new scala.util.Random
-    secondPlayerCard = cardList(r2.nextInt(cardCount))
+    secondPlayerCard = cardList(r.nextInt(cardCount))
     cardCount -= 1
     cardList -= secondPlayerCard
 
@@ -86,19 +84,22 @@ object BlackJack extends App {
       print(s"Your cards: A and $secondPlayerCard. ")
     }
 
-
     playerTotal = firstPlayerCard + secondPlayerCard
     println("Your total: " + playerTotal)
+
+    if (playerTotal == 21) {
+      println("BLACKJACK!")
+      result()
+    }
     playerTotal
 
   }
 
   def hit(): Unit = {
     drawCount += 1
-    val r3 = new scala.util.Random
-    var hitCard = cardList(r3.nextInt(cardCount))
+    var hitCard = cardList(r.nextInt(cardCount))
     cardCount -= 1
-    println(s"$hitCard drawn")
+    println(s"You draw a $hitCard")
 
     playerTotal += hitCard
 
@@ -114,10 +115,25 @@ object BlackJack extends App {
 
     if (playerTotal > 21) bust()
     else if (playerTotal == 21) {
-      println("BLACKJACK!")
       result()
     }
     else choice()
+  }
+
+  def dealerHit(): Unit = {
+    if (dealerTotal > playerTotal && dealerTotal <= 21) result()
+    else if (dealerTotal >= 17 && dealerTotal <= 21) result()
+    else if (dealerTotal > 21) dealerBust()
+    else {
+      dealerDrawCount += 1
+      var dealerHitCard = cardList(r.nextInt(cardCount))
+      println(s"Dealer draws a $dealerHitCard")
+      cardCount -= 1
+      dealerTotal += dealerHitCard
+      println(s"Dealer's total: $dealerTotal")
+      Thread.sleep(1000)
+      dealerHit()
+    }
   }
 
   def choice(): Unit = {
@@ -130,7 +146,15 @@ object BlackJack extends App {
     val decision = scala.io.StdIn.readLine().toLowerCase()
 
     decision match {
-      case "stick" => result()
+      case "stick" =>
+        if (firstCompCard == 11 || firstCompCard == 1){
+          println(s"Dealer's cards: A and $secondCompCard. Total: $dealerTotal ")
+        }
+        if (secondCompCard == 11 || secondCompCard == 1){
+          println(s"Dealer's cards: $firstCompCard and A. Total: $dealerTotal ")
+        }
+        else println(s"Dealer's cards: $firstCompCard and $secondCompCard. Total: $dealerTotal ")
+        dealerHit()
       case "twist" => hit()
       case "hit me" => hit()
       case _ => println("Please enter a valid option")
@@ -141,16 +165,18 @@ object BlackJack extends App {
 
   def result(): Unit = {
 
-    println(s"Dealer's cards: $firstCompCard and $secondCompCard. Total: $dealerTotal ")
-
     if (playerTotal > dealerTotal) {
       println("You win!")
       winCount += 1
-      println(s"$winCount in a row!")
+      println(s"$winCount wins in a row!")
     }
     else if (dealerTotal > playerTotal) {
       println("Dealer wins!")
       winCount = 0
+    }
+    else if (playerTotal == 21 && dealerTotal == 21) {
+      if (drawCount == 5) println("5 cards drawn, you win!")
+      println(s"$winCount wins in a row!")
     }
     else if (dealerTotal == playerTotal) println("No winner")
 
@@ -159,17 +185,26 @@ object BlackJack extends App {
 
   def bust(): Unit = {
     println("BUST! Try again? (Y or N)")
+    winCount = 0
+    playAgain()
+  }
+
+  def dealerBust(): Unit = {
+    println("Dealer BUSTS! You win!")
+    winCount += 1
+    println(s"$winCount wins in a row!")
     playAgain()
   }
 
   def gameLoop(): Unit = {
-
     drawCount = 2
+    dealerDrawCount = 2
     dealerCards()
+    Thread.sleep(1000)
     println()
     playerCards()
+    Thread.sleep(1000)
     choice()
-
   }
 
   def playAgain(): Unit = {
@@ -178,7 +213,7 @@ object BlackJack extends App {
     again match {
       case "y" => gameLoop()
         if (cardCount == 0) println("Out of cards.. sorry!")
-      case "n" => println("ok bye")
+      case "n" => println("ok bye" + cardList)
       case _ => println("Please enter a valid option")
         playAgain()
     }
